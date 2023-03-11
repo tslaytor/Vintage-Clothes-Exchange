8c6dc49b61593d5d13c1b03b48175d57cc09404a
Tom <tom.slaytor@gmail.com>
2023-03-11 12:50:24 +0200

    made logout form

--- src/Models/PageElements/Forms/LogoutForm.php
+++ src/Models/PageElements/Forms/LogoutForm.php
@@ -2,19 +2,14 @@
 
 namespace App\Models\PageElements\Forms;
 
-class LoginForm extends OuterForm
+class LogoutForm extends OuterForm
 {
     public static function generate(): string
     {
         $content = <<<EOF
-            <label for='username' class="form-label">User Name</label><br>
-            <input type='text' id='username' name="username" class="form-control"><br>
-            <label for='password' class="form-label">Password</label><br>
-            <input type='password' id='password' name="password" class="form-control"><br>
-            <input type="submit" class="btn btn-primary" value="Register">
+            <input type="submit" class="btn btn-primary" value="Log Out">
         EOF;
-
-        return OuterForm::generateOuterForm('POST','','LoginForm', $content);
+        return OuterForm::generateOuterForm('POST','','LogoutForm', $content);
     }
 }
 ?>
